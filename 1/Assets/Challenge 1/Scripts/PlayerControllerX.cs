405b4c5c2e6786caf09cb7efccd26a1109ce2c5c
Nico Lerchl <nico@lerchl.one>
2023-03-24 11:54:01 +0100

    Challenge 1: Plane flies forward

--- 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,27 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerControllerX : MonoBehaviour
-{
+public class PlayerControllerX : MonoBehaviour {
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // /////////////////////////////////////////////////////////////////////////
+    // Methods
+    // /////////////////////////////////////////////////////////////////////////
 
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
