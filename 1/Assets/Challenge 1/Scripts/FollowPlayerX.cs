e175e365acf85a5e722cf5ef69ae21a0ab0ad407
Nico Lerchl <nico@lerchl.one>
2023-03-24 12:03:28 +0100

    Challenge 1: Camera follows player

--- 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
-{
+public class FollowPlayerX : MonoBehaviour {
+
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new(25, 0, 5);
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // /////////////////////////////////////////////////////////////////////////
+    // Methods
+    // /////////////////////////////////////////////////////////////////////////
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         transform.position = plane.transform.position + offset;
     }
 }
