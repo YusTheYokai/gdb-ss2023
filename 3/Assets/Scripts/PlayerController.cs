48f08502c79541eb817d95c31a8a71bf8ff7d7f3
Nico Lerchl <nico@lerchl.one>
2023-04-18 12:39:55 +0200

    Lesson 3.4

--- 3/Assets/Scripts/PlayerController.cs
+++ 3/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -5,8 +6,16 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
     public float gravityModifier;
 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
     private Rigidbody rb;
     private Animator anim;
+    private AudioSource audioSource;
+
     private bool onGround = true;
     public bool GameOver { get; private set; } = false;
 
@@ -17,6 +26,7 @@ public class PlayerController : MonoBehaviour {
     void Start() {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -25,16 +35,22 @@ public class PlayerController : MonoBehaviour {
             onGround = false;
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             anim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            audioSource.PlayOneShot(jumpSound, 0.05f);
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")) {
             onGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             GameOver = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
             anim.SetBool("Death_b", true);
             anim.SetInteger("DeathType_int", 1);
+            audioSource.PlayOneShot(crashSound, 0.05f);
             Debug.Log("Game Over!");
         }
     }
